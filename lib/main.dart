be0d43ca7855642906e7245ba714f26eed78b355
WillyOB45 <chukswilly45@gmail.com>
2024-06-24 21:22:45 +0100

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,17 @@
-import "package:flutter/material.dart";
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:image_picker/image_picker.dart';
-import 'package:smooth_page_indicator/smooth_page_indicator.dart';
-
-final countProvider = StateProvider<int>((ref) => 0);
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 void main() {
-  runApp(const ProviderScope(child: myAPP()));
+  runApp(const MyApp());
 }
 
-class myAPP extends StatelessWidget {
-  const myAPP({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      home: homePage(),
-    );
-  }
-}
-
-class homePage extends StatefulWidget {
-  const homePage({super.key});
-
-  @override
-  State<homePage> createState() => _homePageState();
-}
-
-class _homePageState extends State<homePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.purple,
     );
   }
 }
